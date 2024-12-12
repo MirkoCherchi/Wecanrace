fd501ca814bfe46fdd1679829581107ad65be894
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-12-12 13:20:04 +0100

    feat: style

--- wecanrace/src/app/pages/home-page/home-page.component.ts
+++ wecanrace/src/app/pages/home-page/home-page.component.ts
@@ -2,12 +2,15 @@ import { Component } from '@angular/core';
 import {MatCardModule} from '@angular/material/card';
 import {MatDividerModule} from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
+import { RouterLink } from '@angular/router';
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 
 @Component({
   selector: 'app-home-page',
-  imports: [ MatCardModule, MatDividerModule, MatButtonModule],
+  imports: [ MatCardModule, MatDividerModule, MatButtonModule, RouterLink, MatIconModule],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
