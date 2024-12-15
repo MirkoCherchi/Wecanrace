ed578ab9b0af696fe6a765b421d38b0db65ed297
MirkoCherchi <mirkocherchi1992@gmail.com>
2024-12-15 22:54:32 +0100

    feat: fix login and register

--- wecanrace/src/app/components/login/login.component.ts
+++ wecanrace/src/app/components/login/login.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { HttpRequestService } from '../../service/http-request.service';
-
-
-
 
 @Component({
   selector: 'app-login',
@@ -14,7 +16,6 @@ import { HttpRequestService } from '../../service/http-request.service';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
-  
   loginForm = new FormGroup({
     email: new FormControl('', [Validators.required, Validators.email]),
     password: new FormControl('', [Validators.required]),
@@ -22,10 +23,9 @@ export class LoginComponent {
 
   constructor(private httpRequest: HttpRequestService) {}
 
-  
   onSubmit() {
     const loginData = this.loginForm.value;
-    console.log('Login Data:', loginData);  
+    console.log('Login Data:', loginData);
     this.httpRequest.postLogin(loginData).subscribe(
       (response) => {
         console.log('Login response:', response);
@@ -35,6 +35,4 @@ export class LoginComponent {
       }
     );
   }
-  
-  
 }
--- wecanrace/src/app/pages/home-page/home-page.component.ts
+++ wecanrace/src/app/pages/home-page/home-page.component.ts
@@ -1,23 +1,34 @@
 import { Component } from '@angular/core';
-import {MatCardModule} from '@angular/material/card';
-import {MatDividerModule} from '@angular/material/divider';
+import { MatCardModule } from '@angular/material/card';
+import { MatDividerModule } from '@angular/material/divider';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatTabsModule} from '@angular/material/tabs';
+import { MatTabsModule } from '@angular/material/tabs';
 import { LoginComponent } from '../../components/login/login.component';
 import { RegisterComponent } from '../../components/register/register.component';
 
-
-
-
-
-
 @Component({
   selector: 'app-home-page',
-  imports: [ MatCardModule, MatDividerModule, MatButtonModule, MatIconModule, MatTabsModule, LoginComponent, RegisterComponent],
+  imports: [
+    MatCardModule,
+    MatDividerModule,
+    MatButtonModule,
+    MatIconModule,
+    MatTabsModule,
+    LoginComponent,
+    RegisterComponent,
+  ],
   templateUrl: './home-page.component.html',
-  styleUrl: './home-page.component.scss'
+  styleUrl: './home-page.component.scss',
 })
 export class HomePageComponent {
+  message: string = 'Please login to continue';
 
+  onTabChange(index: number): void {
+    if (index === 0) {
+      this.message = 'Please login to continue';
+    } else if (index === 1) {
+      this.message = 'Please register to login';
+    }
+  }
 }
